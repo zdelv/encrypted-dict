136cf1281845a72a0ccdb297b0cef92e354ab294
Zach DelVecchio <zachdelvecchio@gmail.com>
2025-04-04 22:00:17 -0400

    Update install script

--- install.sh
+++ install.sh
@@ -3,35 +3,58 @@
 while [[ $# -gt 0 ]]; do
   case $1 in
     --git)
-      SSH_CONFIG=$2
+      SSH_CONFIG="$2"
       shift
       shift
       ;;
     --install-dir)
-      INSTALL_DIR=$2
+      INSTALL_DIR="$2"
       shift
+      shift
+      ;;
+    --uninstall)
+      UNINSTALL=1
       shift
       ;;
     *)
-      echo "Unknown argument"
+      echo "Unknown argument $1"
       exit 1
       ;;
   esac
-end
+done
+
+if [ -n "$UNINSTALL" ]; then
+  DIR=$(dirname $(dirname $(dirname $(realpath $(which encdb)))))
+  read -p "Uninstall encrypted-dict (located at $DIR)? [y/N]: " USER_INPUT
+  if [ -n "$USER_INPUT" ] && [[ $USER_INPUT == [Yy] ]]; then
+    rm ~/.local/bin/encdb
+    rm -rf DIR
+    exit 1
+  else
+    echo "Exiting without uninstalling"
+    exit 1
+  fi
+fi
 
 if [ -z ${INSTALL_DIR+x} ]; then
   INSTALL_DIR=/usr/local/encrypted_dict
 fi
 
 if [ -z ${SSH_CONFIG+x} ]; then
-  git clone https://github.com/zdelv/encrypted_dict $INSTALL_DIR
+  git clone https://github.com/zdelv/encrypted-dict $INSTALL_DIR
 else
-  git clone git@${SSH_CONFIG}:zdelv/encrypted_dict $INSTALL_DIR
+  git clone git@${SSH_CONFIG}:zdelv/encrypted-dict $INSTALL_DIR
+fi
+
+if [[ $? -ne 0 ]]; then
+  echo "Failed to pull repo"
+  exit 1
 fi
 
 cd $INSTALL_DIR
 python3 -m venv .venv
-pip install .
+.venv/bin/pip install .
+ln -s $(realpath .venv/bin/encdb) ~/.local/bin/encdb
 echo "Installed to /usr/local/encrypted_dict."
 echo "Added symlink to encdb to /usr/local/bin/"
 echo ""
